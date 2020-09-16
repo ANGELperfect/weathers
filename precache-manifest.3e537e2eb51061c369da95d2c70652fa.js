43812e866625b26082b89f7f1b30b5e0508b6f07
Your Name <you@example.com>
2020-09-16 17:05:55 +0100

    Updates

---
+++ precache-manifest.3e537e2eb51061c369da95d2c70652fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e538a12d06a60584650",
+    "url": "/weathers/static/css/main.76940dcd.chunk.css"
+  },
+  {
+    "revision": "1e538a12d06a60584650",
+    "url": "/weathers/static/js/main.83c03f48.chunk.js"
+  },
+  {
+    "revision": "988df4143604ef11f98c",
+    "url": "/weathers/static/js/runtime~main.e3ab5cba.js"
+  },
+  {
+    "revision": "b33bef510142b9ce851f",
+    "url": "/weathers/static/js/2.e3d5bc46.chunk.js"
+  },
+  {
+    "revision": "aa422b9551c6e24d55afe7e49a50da1e",
+    "url": "/weathers/index.html"
+  }
+];
